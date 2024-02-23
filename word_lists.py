033f9525d98ba301ec0c8624b89044bc9a39e916
Minecraft-2009 <124087796+Minecraft-2009@users.noreply.github.com>
2024-02-23 18:59:24 +0000

    <Mostly functional Hangman code.>

--- word_lists.py
+++ word_lists.py
@@ -1,3 +1,7 @@
-animal_words = "ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra".split(
-    " "
-)
+# These are split at " "
+animal_words = "ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra".split(" ")
+
+easy = "hello get good thou fool guess who joe word dead nope yes book essay foolish faggot".split(" ")
+medium = "jokingly liberty treatise republican democratic".split(" ")
+hard = "authoritarian foolhearty characteristic".split(" ")
+impossible = "supercalifragilisticexpialidocious pneumonoultramicroscopicsilicovolcanoconiosis hippopotomonstrosesquippedaliophobia pseudopseudohypoparathyroidism floccinaucinihilipilification".split(" ")
